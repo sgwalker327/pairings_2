0d67aeb26e6c4228e778d331c95f16af4f607f78
Sam Walker <sg.walker327@gmail.com>
2022-11-02 17:00:56 -0600

    Add another variable and elseif

--- friends.rb
+++ friends.rb
@@ -1,6 +1,11 @@
-friend = "bobby"
+friend = "David"
+friend2 = "Steve"
 if friend == "David"
     puts "Hey best friend!"
+elsif friend2 == "Steve" 
+    puts "Hey brother."
 else
     puts "Hey guy."
 end
+
+
